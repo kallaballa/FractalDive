bd6e2d264b65737af8bf08acbfd76feafe2ccc1e
kallaballa <amir@viel-zu.org>
2020-04-20 02:41:46 +0200

    javascript experiments to start off

---
+++ js-experiments/mandelbrot-fractal.js
@@ -0,0 +1,314 @@
+var arrColors = new Array("#FF4848","#FF68DD","#FF62B0","#FE67EB","#E469FE","#D568FD","#9669FE","#FF7575","#FF79E1","#FF73B9","#FE67EB","#E77AFE","#D97BFD","#A27AFE","#FF8A8A","#FF86E3","#FF86C2","#FE8BF0","#EA8DFE","#DD88FD","#AD8BFE","#FF9797","#FF97E8","#FF97CB","#FE98F1","#ED9EFE","#E29BFD","#B89AFE","#FFA8A8","#FFACEC","#FFA8D3","#FEA9F3","#EFA9FE","#E7A9FE","#C4ABFE","#FFBBBB","#FFACEC","#FFBBDD","#FFBBF7","#F2BCFE","#EDBEFE","#D0BCFE","#FFCECE","#FFC8F2","#FFC8E3","#FFCAF9","#F5CAFF","#F0CBFE","#DDCEFF","#FFDFDF","#FFDFF8","#FFDFEF","#FFDBFB","#F9D9FF","#F4DCFE","#E6DBFF","#FFECEC","#FFEEFB","#FFECF5","#FFEEFD","#FDF2FF","#FAECFF","#F1ECFF","#FFF2F2","#FFFEFB","#FFF9FC","#FFF9FE","#FFFDFF","#FDF9FF","#FBF9FF","#800080","#872187","#9A03FE","#892EE4","#3923D6","#2966B8","#23819C","#BF00BF","#BC2EBC","#A827FE","#9B4EE9","#6755E3","#2F74D0","#2897B7","#DB00DB","#D54FD5","#B445FE","#A55FEB","#8678E9","#4985D6","#2FAACE","#F900F9","#DD75DD","#BD5CFE","#AE70ED","#9588EC","#6094DB","#44B4D5","#FF4AFF","#DD75DD","#C269FE","#AE70ED","#A095EE","#7BA7E1","#57BCD9","#FF86FF","#E697E6","#CD85FE","#C79BF2","#B0A7F1","#8EB4E6","#7BCAE1","#FFA4FF","#EAA6EA","#D698FE","#CEA8F4","#BCB4F3","#A9C5EB","#8CD1E6","#FFBBFF","#EEBBEE","#DFB0FF","#DBBFF7","#CBC5F5","#BAD0EF","#A5DBEB","#FFCEFF","#F0C4F0","#E8C6FF","#E1CAF9","#D7D1F8","#CEDEF4","#B8E2EF","#FFDFFF","#F4D2F4","#EFD7FF","#EDDFFB","#E3E0FA","#E0EAF8","#C9EAF3","#FFECFF","#F4D2F4","#F9EEFF","#F5EEFD","#EFEDFC","#EAF1FB","#DBF0F7","#FFF9FF","#FDF9FD","#FEFDFF","#FEFDFF","#F7F5FE","#F8FBFE","#EAF7FB","#5757FF","#62A9FF","#62D0FF","#06DCFB","#01FCEF","#03EBA6","#01F33E","#6A6AFF","#75B4FF","#75D6FF","#24E0FB","#1FFEF3","#03F3AB","#0AFE47","#7979FF","#86BCFF","#8ADCFF","#3DE4FC","#5FFEF7","#33FDC0","#4BFE78","#8C8CFF","#99C7FF","#99E0FF","#63E9FC","#74FEF8","#62FDCE","#72FE95","#9999FF","#99C7FF","#A8E4FF","#75ECFD","#92FEF9","#7DFDD7","#8BFEA8","#AAAAFF","#A8CFFF","#BBEBFF","#8CEFFD","#A5FEFA","#8FFEDD","#A3FEBA","#BBBBFF","#BBDAFF","#CEF0FF","#ACF3FD","#B5FFFC","#A5FEE3","#B5FFC8","#CACAFF","#D0E6FF","#D9F3FF","#C0F7FE","#CEFFFD","#BEFEEB","#CAFFD8","#E1E1FF","#DBEBFF","#ECFAFF","#C0F7FE","#E1FFFE","#BDFFEA","#EAFFEF","#EEEEFF","#ECF4FF","#F9FDFF","#E6FCFF","#F2FFFE","#CFFEF0","#EAFFEF","#F9F9FF","#F9FCFF","#FDFEFF","#F9FEFF","#FDFFFF","#F7FFFD","#F9FFFB","#1FCB4A","#59955C","#48FB0D","#2DC800","#59DF00","#9D9D00","#B6BA18","#27DE55","#6CA870","#79FC4E","#32DF00","#61F200","#C8C800","#CDD11B","#4AE371","#80B584","#89FC63","#36F200","#66FF00","#DFDF00","#DFE32D","#7CEB98","#93BF96","#99FD77","#52FF20","#95FF4F","#FFFFAA","#EDEF85","#93EEAA","#A6CAA9","#AAFD8E","#6FFF44","#ABFF73","#FFFF84","#EEF093","#A4F0B7","#B4D1B6","#BAFEA3","#8FFF6F","#C0FF97","#FFFF99","#F2F4B3","#BDF4CB","#C9DECB","#CAFEB8","#A5FF8A","#D1FFB3","#FFFFB5","#F5F7C4","#D6F8DE","#DBEADC","#DDFED1","#B3FF99","#DFFFCA","#FFFFC8","#F7F9D0","#E3FBE9","#E9F1EA","#EAFEE2","#D2FFC4","#E8FFD9","#FFFFD7","#FAFBDF","#E3FBE9","#F3F8F4","#F1FEED","#E7FFDF","#F2FFEA","#FFFFE3","#FCFCE9","#FAFEFB","#FBFDFB","#FDFFFD","#F5FFF2","#FAFFF7","#FFFFFD","#FDFDF0","#BABA21","#C8B400","#DFA800","#DB9900","#FFB428","#FF9331","#FF800D","#E0E04E","#D9C400","#F9BB00","#EAA400","#FFBF48","#FFA04A","#FF9C42","#E6E671","#E6CE00","#FFCB2F","#FFB60B","#FFC65B","#FFAB60","#FFAC62","#EAEA8A","#F7DE00","#FFD34F","#FFBE28","#FFCE73","#FFBB7D","#FFBD82","#EEEEA2","#FFE920","#FFDD75","#FFC848","#FFD586","#FFC48E","#FFC895","#F1F1B1","#FFF06A","#FFE699","#FFD062","#FFDEA2","#FFCFA4","#FFCEA2","#F4F4BF","#FFF284","#FFECB0","#FFE099","#FFE6B5","#FFD9B7","#FFD7B3","#F7F7CE","#FFF7B7","#FFF1C6","#FFEAB7","#FFEAC4","#FFE1C6","#FFE2C8","#F9F9DD","#FFF9CE","#FFF5D7","#FFF2D2","#FFF2D9","#FFEBD9","#FFE6D0","#FBFBE8","#FFFBDF","#FFFAEA","#FFF9EA","#FFF7E6","#FFF4EA","#FFF1E6","#FEFEFA","#FFFEF7","#FFFDF7","#FFFDF9","#FFFDF9","#FFFEFD","#FFF9F4","#D1D17A","#C0A545","#C27E3A","#C47557","#B05F3C","#C17753","#B96F6F","#D7D78A","#CEB86C","#C98A4B","#CB876D","#C06A45","#C98767","#C48484","#DBDB97","#D6C485","#D19C67","#D29680","#C87C5B","#D0977B","#C88E8E","#E1E1A8",
+"#DECF9C","#DAAF85","#DAA794","#CF8D72","#DAAC96","#D1A0A0","#E9E9BE","#E3D6AA","#DDB791","#DFB4A4","#D69E87","#E0BBA9","#D7ACAC","#EEEECE","#EADFBF","#E4C6A7","#E6C5B9","#DEB19E","#E8CCBF","#DDB9B9","#E9E9C0","#EDE4C9","#E9D0B6","#EBD0C7","#E4C0B1","#ECD5CA","#E6CCCC","#EEEECE","#EFE7CF","#EEDCC8","#F0DCD5","#EACDC1","#F0DDD5","#ECD9D9","#F1F1D6","#F5EFE0","#F2E4D5","#F5E7E2","#F0DDD5","#F5E8E2","#F3E7E7","#F5F5E2","#F9F5EC","#F9F3EC","#F9EFEC","#F5E8E2","#FAF2EF","#F8F1F1","#FDFDF9","#FDFCF9","#FCF9F5","#FDFAF9","#FDFAF9","#FCF7F5","#FDFBFB","#F70000","#B9264F","#990099","#74138C","#0000CE","#1F88A7","#4A9586","#FF2626","#D73E68","#B300B3","#8D18AB","#5B5BFF","#25A0C5","#5EAE9E","#FF5353","#DD597D","#CA00CA","#A41CC6","#7373FF","#29AFD6","#74BAAC","#FF7373","#E37795","#D900D9","#BA21E0","#8282FF","#4FBDDD","#8DC7BB","#FF8E8E","#E994AB","#FF2DFF","#CB59E8","#9191FF","#67C7E2","#A5D3CA","#FFA4A4","#EDA9BC","#F206FF","#CB59E8","#A8A8FF","#8ED6EA","#C0E0DA","#FFB5B5","#F0B9C8","#FF7DFF","#D881ED","#B7B7FF","#A6DEEE","#CFE7E2","#FFC8C8","#F4CAD6","#FFA8FF","#EFCDF8","#C6C6FF","#C0E7F3","#DCEDEA","#FFEAEA","#F8DAE2","#FFC4FF","#EFCDF8","#DBDBFF","#D8F0F8","#E7F3F1","#FFEAEA","#FAE7EC","#FFE3FF","#F8E9FC","#EEEEFF","#EFF9FC","#F2F9F8","#FFFDFD","#FEFAFB","#FFFDFF","#FFFFFF","#FDFDFF","#FAFDFE","#F7FBFA");
+
+// ------------------------------------------------------------------------
+// Based on:
+// http://rembound.com/articles/drawing-mandelbrot-fractals-with-html5-canvas-and-javascript
+// ------------------------------------------------------------------------
+var imagew = 512;
+var imageh = 512;
+var frameBufferSize = imagew * imageh;
+var imagedata;
+var greydata = new Array(frameBufferSize);
+
+var offsetx;
+var offsety;
+// Pan and zoom parameters
+var panx = 100;
+var pany = 182;
+var zoom = 30;
+
+// Palette array of 256 colors
+var palette = [];
+
+// The maximum number of iterations per pixel
+var maxiterations = 250;
+
+var canvas;
+var context;
+
+function entropy(arr) {
+ var len = arr.length
+ 
+  // Build a frequency map from the string.
+  var frequencies = arr
+    .reduce((freq, val) => (freq[val] = (freq[val] || 0) + 1) && freq, {})
+ 
+  // Sum the frequency of each character.
+  return Object.values(frequencies)
+    .reduce((sum, f) => sum - f/len * Math.log2(f/len), 0)
+};
+
+function measureDetail(arr) {
+//        return entropy(arr);
+    var clrs = new Set();
+    var zeroes = 0;
+    for(var i = 0; i < arr.length; i++) {
+        clrs.add(arr[i]);
+        if(arr[i] === 0)
+            zeroes++;
+    }
+  
+    return ((clrs.size / 255.0) + (1.0 - (zeroes / arr.length))) / 2;
+}
+
+function initFrameBuffer() {
+    offsetx = -imagew/2;
+    offsety = -imageh/2;
+
+    // Image Data (RGBA)
+    imagedata = context.createImageData(imagew, imageh);
+}
+
+// Initialize the game
+function init() {
+    // Add mouse events
+    canvas.addEventListener("mousedown", onMouseDown);
+    
+    // Generate palette
+    generatePalette();
+    
+    // Generate image
+    generateImage(false);
+
+    // Enter main loop
+    main(0);
+}
+
+
+var shiftIncrement = 5;
+var shiftxRemainder = 0;
+var shiftyRemainder = 0;
+
+// Main loop
+function main(tframe) {
+    // Request animation frames
+    window.requestAnimationFrame(main);
+    var detail = measureDetail(greydata);
+    if(detail > 0.9) {
+        shiftxRemainder = 0;
+        shiftyRemainder = 0;
+        zoomFractalAtCenter(1.05, true);
+        generateImage(false);
+        context.putImageData(imagedata, 0, 0);
+        return;
+    }
+        
+    if(shiftxRemainder !== 0 || shiftyRemainder !== 0) {
+        var shiftx = 0;
+        var shifty = 0;
+
+        if(shiftxRemainder < 0) {
+            shiftx = Math.max(shiftxRemainder, -5);
+        } else if(shiftxRemainder > 0) {
+            shiftx = Math.min(shiftxRemainder, 5);
+        }
+
+        if(shiftyRemainder < 0) {
+            shifty = Math.max(shiftyRemainder, -5);
+        } else if(shiftyRemainder > 0) {
+            shifty = Math.min(shiftyRemainder, 5);
+        }
+
+        shiftxRemainder -= shiftx;
+        shiftyRemainder -= shifty;
+        
+        panRelative(shiftx, shifty);
+        generateImage(false);
+        context.putImageData(imagedata, 0, 0);
+        return;
+    } else {
+        var shiftx = 0;
+        var shifty = 0;
+        var ent = 0;
+        var lastShiftx = 0;
+        var lastShifty = 0;
+        var candidateShiftx = 0;
+        var candidateShifty = 0;   
+        
+        zoomFractalAtCenter(1.05,true);        
+        generateImage(true);
+        var initialent = measureDetail(greydata);
+        for(var i = 0; i < 3; ++i) {
+            shiftx = parseInt(Math.random() * 40) - 20;
+            shifty = parseInt(Math.random() * 40) - 20;
+            panRelative(-lastShiftx, -lastShifty);
+            panRelative(shiftx, shifty);
+            // Generate a new image
+            generateImage(true);
+            ent = measureDetail(greydata);
+
+            if(ent > initialent) {
+                candidateShiftx = shiftx;
+                candidateShifty = shifty;
+                break;
+            }
+            lastShiftx = shiftx;
+            lastShifty = shifty;
+        }
+        panRelative(-shiftx, -shifty);
+
+        if(candidateShiftx !== 0 || candidateShifty !== 0) {
+            shiftxRemainder = candidateShiftx;
+            shiftyRemainder = candidateShifty;
+        } else {
+            zoomFractalAtCenter(1.05, true);
+            generateImage(false);
+            context.putImageData(imagedata, 0, 0);
+        }
+    }
+}
+
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+// Generate palette
+function generatePalette() {
+    // Calculate a gradient
+    var increment = arrColors.length / 256;
+    for (var i=0; i<256; i++) {
+        palette[i] = hexToRgb(arrColors[Math.ceil(i * increment)]);
+//        palette[i] = hexToRgb(arrColors[parseInt(Math.random() * arrColors.length)]);
+    }
+}
+
+// Generate the fractal image
+function generateImage(greyonly) {
+    for(var i = 0; i < greydata.length; i++) {
+        greydata[i] = 0;
+    }
+
+    // Iterate over the pixels
+    for (var y=0; y<imageh; y++) {
+        for (var x=0; x<imagew; x++) {
+            iterate(x, y, maxiterations, greyonly);
+        }
+    }
+}
+
+// Calculate the color of a specific pixel
+function iterate(x, y, maxiterations, greyonly) {
+    // Convert the screen coordinate to a fractal coordinate
+    var x0 = (x + offsetx + panx) / zoom;
+    var y0 = (y + offsety + pany) / zoom;
+    
+    // Iteration variables
+    var a = 0;
+    var b = 0;
+    var rx = 0;
+    var ry = 0;
+    
+    // Iterate
+    var iterations = 0;
+    while (iterations < maxiterations && (rx * rx + ry * ry <= 4)) {
+        rx = a * a - b * b + x0;
+        ry = 2 * a * b + y0;
+        
+        // Next iteration
+        a = rx;
+        b = ry;
+        iterations++;
+    }
+    
+    // Get palette color based on the number of iterations
+    var color;
+    var index = 0;
+    if (iterations === maxiterations) {
+        color = { r:0, g:0, b:0}; // Black
+    } else {
+        index = Math.floor((iterations / (maxiterations-1)) * 255);
+        color = palette[index];
+    }
+    
+    // Apply the color
+    if(!greyonly) {
+        var pixelindex = (y * imagew + x) * 4;
+        imagedata.data[pixelindex] = color.r;
+        imagedata.data[pixelindex+1] = color.g;
+        imagedata.data[pixelindex+2] = color.b;
+        imagedata.data[pixelindex+3] = 255;
+    }
+
+    if (iterations === maxiterations) {
+        greydata[y * imagew + x] = 0;
+    }
+    else {
+        greydata[y * imagew + x] = index; //cheap greyscale. we don't need perceptual acuity to measure detail.
+    }
+}
+
+// Zoom the fractal
+function zoomFractalAt(x, y, factor, zoomin) {
+    if (zoomin) {
+        // Zoom in
+        zoom *= factor;
+        panx = factor * (x + offsetx + panx);
+        pany = factor * (y + offsety + pany);
+    } else {
+        // Zoom out
+        zoom /= factor;
+        panx = (x + offsetx + panx) / factor;
+        pany = (y + offsety + pany) / factor;
+    }
+}
+
+function zoomFractalAtCenter(factor, zoomin) {
+    if (zoomin) {
+        // Zoom in
+        zoom *= factor;
+    } else {
+        // Zoom out
+        zoom /= factor;
+    }
+}
+
+function panRelative(x, y) {
+        panx += x;
+        pany += y;
+}
+
+// Mouse event handlers
+function onMouseDown(e) {
+    var pos = getMousePos(canvas, e);
+    
+    // Zoom out with Control
+    var zoomin = true;
+    if (e.ctrlKey) {
+        zoomin = false;
+    }
+    
+    // Pan with Shift
+    var zoomfactor = 2;
+    if (e.shiftKey) {
+        zoomfactor = 1;
+    }
+    
+    // Zoom the fractal at the mouse position
+    zoomFractal(pos.x, pos.y, zoomfactor, zoomin);
+    
+    // Generate a new image
+    generateImage();
+}
+
+// Get the mouse position
+function getMousePos(canvas, e) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: Math.round((e.clientX - rect.left)/(rect.right - rect.left)*canvas.width),
+        y: Math.round((e.clientY - rect.top)/(rect.bottom - rect.top)*canvas.height)
+    };
+}
+
+
+window.onload = function() {
+    canvas = document.getElementById("viewport"); 
+    context = canvas.getContext("2d");
+    initFrameBuffer();
+    init();
+};
